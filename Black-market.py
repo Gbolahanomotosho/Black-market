a395d8f9c1a9391f7d101626dc1482bd2ebdcac8
Gbolahanomotosho <115882789+Gbolahanomotosho@users.noreply.github.com>
2022-12-13 22:45:46 +0100

    Add files via upload

---
+++ Black-market.py
@@ -0,0 +1,75 @@
+# !/usr/bin/env/python
+#
+#
+# This is an underground market where you buy and sell any of the services and stuff below....
+# Any form of scam in this market is prohibited as this market is free from all form of scam....
+# The currency used in this market is both Monero or Btc
+# Every stuff sold or services rendered in this market is legit
+# Every transaction on this market is strictly private and legit
+# Ensure to obey every market rules and regulation
+# The purpose which this market is created is to reduce the stress of selling stuff and services on a scam platform or on darkweb
+# Also this market is created to stop and reduce any form of scam and make transaction easier
+# Every transaction is this market is safe.....
+# Every programmer, hackers, spammer, fraudster can meet to buy and sell services or goods on this platform 
+# Invite everyone to this market and lets buy and sell to make money 💰💸🤑 on this platform 💰💸🤑
+# Use the correct market token
+#
+#
+
+
+print("   &&&&&&&&&      &&&&&&&&&&       &&&&&&&&&&                  &&&&&&&&     &&&&&&&&&     &&&&&&&&&   ")                              
+print("   &            &                  &         &   &&&&&&&&&     &      &     &       &     &           ") 
+print("    &&&&&&&&    &                  &&&&&&&&&&                  &      &     &&&&&&&&&     &&&&&&&&&   ")        
+print("           &    &                  &                           &      &             &     &       &   ") 
+print("    &&&&&&&&      &&&&&&&&&&       &                           &&&&&&&&             &     &&&&&&&&&   ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+print("                               TELEGRAM UNDER-GROUND MARKET                                           ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+print("                      THE  MARKET-HUB  FOR ALL HACKERS  AND  ALL  HACKING  TOOLS/MALWARE/DATABASE     ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+print("                                                                                                      ")
+print("                           Script coded by Omotosho Gbolahan Hammed                                   ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+print("                                                                                                      ") 
+ 
+
+
+import os
+import time
+import sys
+def start():
+           market_token 
+while True:
+        market_token = '555665346799355343728'
+        market_token = str(input("Enter market_token: "))
+        os.system('clear')
+        time.sleep(0.3)
+        if market_token in ("555665346799355343728"): break
+        else: exit("Dont be a scammer!")
+print("welcome to the SCP-096 telegram market underground forum, we buy and sell any of the stuff and services in the market below, choose any of the stuff or services you want to buy or sell:")
+print("")
+print("")
+print("1. Database\n2. Malware/Virus\n3. Credit card/Debit card\n4. Any login details, like banks log, business log, email log, social media log and any other log and so on\n5. Any Hacking tools and script\n6. Cpanel or web admin access or any other type of web admin access/login\n7. Spamming tools\n8. Hiring of hacker\n9. Ransomware as a service or Ransomware script\n10. Hacking tutorial\n11. Software/app\n12. Sensitive information like driver license, SSN, leaked sex tape and so on\n13. All carding tools/skimmer\n14. Carding tutorial\n15. Botnet/DDos services")
+print("")
+print("")
+number=input("Enter any of the number above:")
+time.sleep(0.5)
+print("")
+print("")
+def MSG():
+	print(Y)
+y = "link"
+GO = input("Do you want to join the market to buy and sell ? (y/n): ")
+if GO == 'Y' or GO == 'y': pass
+                       
+elif GO == 'N' or GO == 'n':
+    n = ('clear')
+print("pls wait while we redirect.... you to our market in 5 seconds")
+time.sleep(5)
+os.system("xdg-open https://t.me/+O1AzhLSKpds4OTc0")
+link = ("xdg-open https://t.me/+O1AzhLSKpds4OTc0")
---
+++ README.md
@@ -0,0 +1,40 @@
+# Black-market underground market
+
+ This is an underground market where you buy and sell any of the services and stuff below....
+ Any form of scam in this market is prohibited as this market is free from all form of scam....
+ The currency used in this market is both Monero or Btc
+ Every stuff sold or services rendered in this market is legit
+ Every transaction on this market is strictly private and legit
+ Ensure to obey every market rules and regulation
+ The purpose which this market is created is to reduce the stress of selling stuff and services on a scam platform or on darkweb
+ Also this market is created to stop and reduce any form of scam and make transaction easier
+ Every transaction is this market is safe.....
+ Every programmer, hackers, spammer, fraudster can meet to buy and sell services or goods on this platform 
+ Invite everyone to this market and lets buy and sell to make money 💰💸🤑 on this platform 💰💸🤑
+ Let everyone buy and sell on this market and lets smile 🤑🤑 to the bank
+ The market bot is in progress and will be ready soon......
+
+# Tested platform
+
+  this script works on all platform like termux, kali, black-arch, parrot
+  cyborg hawk, ubuntu, fedorra, window, macos and so on.......
+
+# Commands/installation
+ 
+  git clone https://github.com/Gbolahanomotosho/Black-market 
+  cd Black-market
+  python3 Black-market.py
+
+# Market token
+  
+  555665346799355343728
+
+# Donation
+
+ donation is needed to maintain this market platform to add more extra features
+ pls donate any amount of Ethereum to
+ my ethereum wallet below 👇👇👇 
+
+ 0x675a288a45106f2e86574b8828e04de4d9d6c316
+
+ thank you as you guys join the market platform to trade........
